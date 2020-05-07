67f5e430beb359e21554af9d20f2bc2302a3a8f7
alenashashko <L.Shashko@mail.ru>
2020-05-07 20:33:00 +0300

    Добавляет js-код для появления модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,11 @@
 var header = document.querySelector('.header');
 var button = header.querySelector('.header__toggle');
+var buttonOrder = document.querySelector(".popular__order");
+var modal = document.querySelector(".modal-order");
+var modalPopup = modal.querySelector(".modal-order__popup");
+var buttons = document.querySelectorAll(".catalog-item__to-basket");
+var modal = document.querySelector(".modal-order");
+var modalPopup = modal.querySelector(".modal-order__popup");
 
 header.classList.remove('header--nojs');
 
@@ -12,3 +18,50 @@ button.addEventListener('click', function () {
     header.classList.add('header--menu-closed');
   }
 });
+
+if (buttonOrder) {
+  buttonOrder.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    console.log("Хелло");
+    modalPopup.classList.remove("modal-close-animation");
+    modal.classList.add("modal-show");
+    modalPopup.classList.add("modal-open-animation");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+
+      if (modal.classList.contains("modal-show")) {
+        timer = setTimeout('modal.classList.remove("modal-show")', 1000);
+        modalPopup.classList.remove("modal-open-animation");
+        modalPopup.classList.add("modal-close-animation");
+      }
+    }
+  });
+}
+
+if (buttons) {
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    console.log("Хелло");
+    modalPopup.classList.remove("modal-close-animation");
+    modal.classList.add("modal-show");
+    modalPopup.classList.add("modal-open-animation");
+    });
+  }
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+
+      if (modal.classList.contains("modal-show")) {
+        timer = setTimeout('modal.classList.remove("modal-show")', 1000);
+        modalPopup.classList.remove("modal-open-animation");
+        modalPopup.classList.add("modal-close-animation");
+      }
+    }
+  });
+}
+
