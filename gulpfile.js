87c8fb281daea450ca4b602399a9188571077de9
alenashashko <L.Shashko@mail.ru>
2020-05-10 20:00:03 +0300

    Выносит keyframes в отдельный файл

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,6 @@ var include = require("posthtml-include");
 var del = require("del");
 var htmlmin = require('gulp-htmlmin');
 var uglify = require('gulp-uglify');
-var pipeline = require('readable-stream').pipeline;
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
